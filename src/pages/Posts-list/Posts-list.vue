14f0c8bdf61d850f4ec6673b4293ab331e96af1b
dmytruk <iryna.zgn@gmail.com>
2018-10-09 23:08:43 +0300

    Attempt infinite scroll

--- src/pages/Posts-list/Posts-list.vue
+++ src/pages/Posts-list/Posts-list.vue
@@ -5,8 +5,8 @@
       v-if="lastPost"
       :post="lastPost"/>
     <blog-preview-posts
-      v-if="remainingPosts.length > 0"
-      :posts="remainingPosts"/>
+      v-if="posts.length > 0"
+      :posts="posts"/>
   </div>
 </template>
 
@@ -20,11 +20,45 @@ export default {
     BlogLastPost,
     BlogPreviewPosts
   },
+  data() {
+    return {
+      page: 0,
+      perPage: 2
+    }
+  },
   computed: {
     ...mapGetters({
       lastPost: 'posts/getLastPost',
       remainingPosts: 'posts/getRemainingPosts'
+    }),
+    posts () {
+      return this.partPosts()
+    }
+  },
+  created () {
+    this.partPosts()
+    window.addEventListener('scroll', () => {
+      if (this.bottomVisible()) {
+        this.partPosts()
+      }
     })
+  },
+  methods: {
+    bottomVisible () {
+      const scrollY = window.scrollY
+      const visible = document.documentElement.clientHeight
+      const pageHeight = document.documentElement.scrollHeight
+      const bottomOfPage = visible + scrollY >= pageHeight
+      return bottomOfPage || pageHeight < visible
+    },
+    partPosts () {
+      // const from = this.page * this.perPage
+      const to = (this.page + 1) * this.perPage
+      const part = this.remainingPosts.slice(0, to)
+      this.page++
+      return part
+      // this.posts.push(...part)
+    }
   }
 }
 </script>
