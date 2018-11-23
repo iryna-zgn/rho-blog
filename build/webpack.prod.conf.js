c1ef3a491cdc81e606ab599ad73dce2dd3e6c518
dmytruk <iryna.zgn@gmail.com>
2018-11-23 02:06:57 +0200

    Add prerender

--- build/webpack.prod.conf.js
+++
@@ -1,8 +0,0 @@
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
-
-/* eslint-disable */
-new PrerenderSpaPlugin(
-  path.join(__dirname, '../docs'),
-  [ '/' ]
-)
-/* eslint-enable */
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
+const path = require('path')
+
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/rho-blog/'
-    : '/'
+  configureWebpack: {
+    plugins: [
+      new PrerenderSpaPlugin(
+        path.join(__dirname, 'docs'),
+        [ '/' ]
+      )
+    ]
+  }
 }
