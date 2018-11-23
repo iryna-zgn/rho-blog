115b112d8d5601a9f50f5d7270666fe7dfb01575
dmytruk <iryna.zgn@gmail.com>
2018-11-23 02:56:32 +0200

    Change config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
-const PrerenderSpaPlugin = require('prerender-spa-plugin')
-const path = require('path')
+// const PrerenderSpaPlugin = require('prerender-spa-plugin')
+// const path = require('path')
 
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/rho-blog/' : '/',
-  configureWebpack: {
-    plugins: [
-      new PrerenderSpaPlugin(
-        path.join(__dirname, 'dist'),
-        [ '/' ]
-      )
-    ]
-  }
+  outputDir: 'docs'
+  // configureWebpack: {
+  //   plugins: [
+  //     new PrerenderSpaPlugin(
+  //       path.join(__dirname, 'docs'),
+  //       [ '/' ]
+  //     )
+  //   ]
+  // }
 }
