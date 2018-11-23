b235e028e97d0b567a1168c3a7e2b44684f365e4
dmytruk <iryna.zgn@gmail.com>
2018-11-23 02:14:45 +0200

    Fix prerender

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,11 @@ const PrerenderSpaPlugin = require('prerender-spa-plugin')
 const path = require('path')
 
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production' ? '/rho-blog/' : '/',
   configureWebpack: {
     plugins: [
       new PrerenderSpaPlugin(
-        path.join(__dirname, 'docs'),
+        path.join(__dirname, 'dist'),
         [ '/' ]
       )
     ]
