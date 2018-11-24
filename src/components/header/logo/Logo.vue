691bcf9c5aa98b603db0b713f1a34341b8ab3516
dmytruk <iryna.zgn@gmail.com>
2018-11-24 02:09:34 +0200

    Corrections

--- src/components/header/logo/Logo.vue
+++ src/components/header/logo/Logo.vue
@@ -4,7 +4,6 @@
       :to="{name: 'postsList'}"
       tag="div"
       class="logo__link">
-      <!-- <img src="/static/logo.svg" alt=""> -->
       <span class="logo__main">rho</span>
       <span class="logo__text">blog</span>
     </router-link >
