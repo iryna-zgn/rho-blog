faff2bf8ca227d2a9de46faaf71964e59695fbeb
dmytruk <iryna.zgn@gmail.com>
2018-11-24 21:53:55 +0200

    Corrections

--- src/store/modules/posts/paths.js
+++ src/store/modules/posts/paths.js
@@ -1,4 +1,2 @@
-// export const posts = '/static/posts/posts.json'
-// export const tagsTranslations = '/static/posts/tagsTranslations.json'
 export const posts = 'static/posts/posts.json'
 export const tagsTranslations = 'static/posts/tagsTranslations.json'
